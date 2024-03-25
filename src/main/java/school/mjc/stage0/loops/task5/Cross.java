232c35d7d5170f18769b589647fa6a77ee6798bc
Artur Kosmovskij <kosmos146139@gmail.com>
2024-03-25 18:12:50 +0300

    12

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int half = (sideLength + 1) / 2;
+        for (int i = 1;i <= sideLength;i++)
+        {
+            for (int j = 1; j <= sideLength;j++)
+            {
+                if(i == half || j == half) {System.out.print('8');}
+                else {System.out.print(' ');}
+                if (j == sideLength) System.out.print('\n');
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,6 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+
     }
 }
